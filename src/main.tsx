46fb0dded0d1cd3373b30d9b5b79acb2644ffdcc
leanforwarddesign <leanforward.designs@gmail.com>
2025-10-25 16:37:36 +1100

    removed api key

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from '@/app.tsx'
-import { maybeShowApiKeyBanner } from '@/gemini-api-banner.ts'
-
-const API_KEY = 'AIzaSyAcFyp4kPrCsUbk0DR9kjmnpxNNcSrMBmY';
+import { App } from '@/app.tsx';
+import { maybeShowApiKeyBanner } from '@/gemini-api-banner.ts';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
